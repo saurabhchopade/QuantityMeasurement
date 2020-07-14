19dfcae00590ba53926becf1f75417abf02766ff
saurabh chopade <saurabhchopade3250@gmail.com>
2020-07-15 00:50:05 +0530

    ADDED : Test to check inch equality

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -1,4 +1,4 @@
-import com.bridgelabz.quantitymeasurement.service.Feet;
+import com.bridgelabz.quantitymeasurement.service.Length;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,31 +7,38 @@ public class QuantityMeasurementTest {
 
     @Test
     public void given0FeetAnd0Feet_ShouldReturnTrue() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
-        Assert.assertEquals(feet1, feet2);
+        Length length1 = new Length(0.0);
+        Length length2 = new Length(0.0);
+        Assert.assertEquals(length1, length2);
     }
 
     @Test
     public void given0FeetAnd1Feet_ShouldReturnFalse() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.1);
-        Assert.assertNotEquals(feet1, feet2);
+        Length length1 = new Length(0.0);
+        Length length2 = new Length(0.1);
+        Assert.assertNotEquals(length1, length2);
     }
 
     @Test
     public void givenNullFeetAnd1Feet_ShouldReturnFalse() {
-        Feet feet1 = null;
-        Feet feet2 = new Feet(0.1);
-        boolean result = feet2.equals(feet1);
+        Length length1 = null;
+        Length length2 = new Length(0.1);
+        boolean result = length2.equals(length1);
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void givenDoubleAnd1Feet_ShouldReturnFalse() {
         double feet1 = 0.1;
-        Feet feet2 = new Feet(0.1);
-        boolean result = feet2.equals(feet1);
+        Length length2 = new Length(0.1);
+        boolean result = length2.equals(feet1);
         Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void given0InchAnd0Inch_ShouldReturnTrue() {
+        Length length1 = new Length(1.0);
+        Length length2 = new Length(1.0);
+        Assert.assertEquals(length1, length2);
     }
 }
