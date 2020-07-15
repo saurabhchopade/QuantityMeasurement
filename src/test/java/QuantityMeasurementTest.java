7a3a0711a2e68ff3a60f3d7a779023f80f055caf
saurabh chopade <saurabhchopade3250@gmail.com>
2020-07-15 22:14:10 +0530

    ADDED : Test to check 1Inch is equal to 2.55Cm or not

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -1,4 +1,4 @@
-import com.bridgelabz.quantitymeasurement.service.Length;
+import com.bridgelabz.quantitymeasurement.service.LengthConverter;
 import com.bridgelabz.quantitymeasurement.util.Units;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,22 +8,22 @@ public class QuantityMeasurementTest {
 
     @Test
     public void given0FeetAnd0Feet_ShouldReturnTrue() {
-        Length length1 = new Length(0.0, Units.FEET);
-        Length length2 = new Length(0.0, Units.FEET);
+        LengthConverter length1 = new LengthConverter(0.0, Units.FEET);
+        LengthConverter length2 = new LengthConverter(0.0, Units.FEET);
         Assert.assertEquals(length1, length2);
     }
 
     @Test
     public void given0FeetAnd1Feet_ShouldReturnFalse() {
-        Length length1 = new Length(0.0, Units.FEET);
-        Length length2 = new Length(0.1, Units.FEET);
+        LengthConverter length1 = new LengthConverter(0.0, Units.FEET);
+        LengthConverter length2 = new LengthConverter(0.1, Units.FEET);
         Assert.assertNotEquals(length1, length2);
     }
 
     @Test
     public void givenNullFeetAnd1Feet_ShouldReturnFalse() {
-        Length length1 = null;
-        Length length2 = new Length(0.1, Units.FEET);
+        LengthConverter length1 = null;
+        LengthConverter length2 = new LengthConverter(0.1, Units.FEET);
         boolean result = length2.equals(length1);
         Assert.assertEquals(false, result);
     }
@@ -31,29 +31,29 @@ public class QuantityMeasurementTest {
     @Test
     public void givenDoubleAnd1Feet_ShouldReturnFalse() {
         double length1 = 0.1;
-        Length length2 = new Length(0.1, Units.FEET);
+        LengthConverter length2 = new LengthConverter(0.1, Units.FEET);
         boolean result = length2.equals(length1);
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void given0InchAnd0Inch_ShouldReturnTrue() {
-        Length length1 = new Length(1.0, Units.INCH);
-        Length length2 = new Length(1.0, Units.INCH);
+        LengthConverter length1 = new LengthConverter(1.0, Units.INCH);
+        LengthConverter length2 = new LengthConverter(1.0, Units.INCH);
         Assert.assertEquals(length1, length2);
     }
 
     @Test
     public void given1InchAnd2Inch_ShouldReturnFalse() {
-        Length length1 = new Length(1.0, Units.INCH);
-        Length length2 = new Length(2.0, Units.INCH);
+        LengthConverter length1 = new LengthConverter(1.0, Units.INCH);
+        LengthConverter length2 = new LengthConverter(2.0, Units.INCH);
         Assert.assertNotEquals(length1, length2);
     }
 
     @Test
     public void givenNullInchAnd1Inch_ShouldReturnFalse() {
-        Length length1 = null;
-        Length length2 = new Length(0.1, Units.INCH);
+        LengthConverter length1 = null;
+        LengthConverter length2 = new LengthConverter(0.1, Units.INCH);
         boolean result = length2.equals(length1);
         Assert.assertEquals(false, result);
     }
@@ -61,63 +61,70 @@ public class QuantityMeasurementTest {
     @Test
     public void givenDoubleAnd1Inch_ShouldReturnFalse() {
         double length1 = 0.1;
-        Length length2 = new Length(0.1, Units.INCH);
+        LengthConverter length2 = new LengthConverter(0.1, Units.INCH);
         boolean result = length2.equals(length1);
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void given3FeetEqualTo1Yard_ShouldReturnTrue() {
-        Length length1 = new Length(3.0, Units.FEET);
-        Length length2 = new Length(1.0, Units.YARD);
+        LengthConverter length1 = new LengthConverter(3.0, Units.FEET);
+        LengthConverter length2 = new LengthConverter(1.0, Units.YARD);
         boolean result = length1.compare(length2);
         Assert.assertTrue(result);
     }
 
     @Test
     public void given1FeetNotEqualTo1Yard_ShouldReturnFalse() {
-        Length length1 = new Length(1.0, Units.FEET);
-        Length length2 = new Length(1.0, Units.YARD);
+        LengthConverter length1 = new LengthConverter(1.0, Units.FEET);
+        LengthConverter length2 = new LengthConverter(1.0, Units.YARD);
         boolean result = length1.compare(length2);
         Assert.assertFalse(result);
     }
 
     @Test
     public void given1InchNotEqualTo1Yard_ShouldReturnFalse() {
-        Length length1 = new Length(1.0, Units.INCH);
-        Length length2 = new Length(1.0, Units.YARD);
+        LengthConverter length1 = new LengthConverter(1.0, Units.INCH);
+        LengthConverter length2 = new LengthConverter(1.0, Units.YARD);
         boolean result = length1.compare(length2);
         Assert.assertFalse(result);
     }
 
     @Test
     public void given1YardEqualTo36inch_ShouldReturnTrue() {
-        Length length1 = new Length(1.0, Units.YARD);
-        Length length2 = new Length(36.0, Units.INCH);
+        LengthConverter length1 = new LengthConverter(1.0, Units.YARD);
+        LengthConverter length2 = new LengthConverter(36.0, Units.INCH);
         boolean result = length1.compare(length2);
         Assert.assertTrue(result);
     }
 
     @Test
     public void given36InchIfEqualTo1Yard_ShouldReturnTrue() {
-        Length length1 = new Length(36.0, Units.INCH);
-        Length length2 = new Length(1.0, Units.YARD);
+        LengthConverter length1 = new LengthConverter(36.0, Units.INCH);
+        LengthConverter length2 = new LengthConverter(1.0, Units.YARD);
         boolean result = length1.compare(length2);
         Assert.assertTrue(result);
     }
 
     @Test
     public void given1YardIfEqualTo3Feet_ShouldReturnTrue() {
-        Length length1 = new Length(1.0, Units.YARD);
-        Length length2 = new Length(3.0, Units.FEET);
+        LengthConverter length1 = new LengthConverter(1.0, Units.YARD);
+        LengthConverter length2 = new LengthConverter(3.0, Units.FEET);
         boolean result = length1.compare(length2);
         Assert.assertTrue(result);
     }
 
     @Test
+    public void given1InchIfEqualTo2Point5Cm_ShouldReturnTrue() {
+        LengthConverter length1 = new LengthConverter(1.0, Units.INCH);
+        LengthConverter length2 = new LengthConverter(2.54, Units.CM);
+        boolean result = length1.compare(length2);
+        Assert.assertTrue(result);
+    }
+    @Test
     public void given2InchIfEqualTo5Cm_ShouldReturnTrue() {
-        Length length1 = new Length(1.0, Units.INCH);
-        Length length2 = new Length(5.0, Units.CM);
+        LengthConverter length1 = new LengthConverter(2.0, Units.INCH);
+        LengthConverter length2 = new LengthConverter(5.08, Units.CM);
         boolean result = length1.compare(length2);
         Assert.assertTrue(result);
     }
