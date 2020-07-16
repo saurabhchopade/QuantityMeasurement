2c87536b008446000658a5c625619ae8f90dd71f
saurabh chopade <saurabhchopade3250@gmail.com>
2020-07-16 18:31:57 +0530

    REFACTORED : Code added new class to accommodate single responsibility

---
+++ src/main/java/com/bridgelabz/quantitymeasurement/service/AddQuantity.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.quantitymeasurement.service;
+
+public class AddQuantity {
+    /**
+     * Add will combine two quantity each other
+     * @param quantity1
+     * @param quantity2
+     * @return
+     */
+    public Double addTwoQuantity(UnitsConverter quantity1, UnitsConverter quantity2) {
+        Double length1 = quantity1.units.getValue() * quantity1.value;
+        Double length2 = quantity2.units.getValue() * quantity2.value;
+        return length1 + length2;
+    }
+}
